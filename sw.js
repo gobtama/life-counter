1211030178d22588a9c77bd4b32ab41a259dd288
gobtama <gobtama99@gmail.com>
2026-09-08 22:12:29 +0900

    Force-refresh updated animation assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "life-counter-v12";
+const CACHE_NAME = "life-counter-v13";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css",
-  "./script.js",
+  "./style.css?v=13",
+  "./script.js?v=13",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
